d6547d1d1429f8238e17a179b2fafed504d6371e
dxinteractive <dxinteractive@gmail.com>
2022-07-02 21:00:37 +1000

    bail on troublesome faust npm package, try code staight from faust repo

--- demo/src/faust-live-player.ts
+++ demo/src/faust-live-player.ts
@@ -20,7 +20,6 @@ export function useFaustLivePlayer(dspDefinition: DspDefinition) {
     faust.compileNode(audioContext, dspDefinition.dsp).then((node) => {
       if (effectCountRef.current !== count) return;
       node.connect(audioContext.destination);
-      node.start();
       audioNode.current = node;
     });
 
--- src/faust.ts
+++ src/faust.ts
@@ -1,4 +1,4 @@
-import { FaustModule, Faust as FaustLibrary } from "./faust/faust-with-types";
+// import { FaustModule, Faust as FaustLibrary } from "./faust/faust-with-types";
 import type {
   MonoFactory,
   Compiler,
@@ -12,71 +12,87 @@ export type ReadyResult = {
 };
 
 export default class Faust {
-  ready: Promise<ReadyResult>;
+  // ready: Promise<ReadyResult>;
 
   constructor() {
-    this.ready = FaustModule().then((module) => {
-      const compiler = FaustLibrary.createCompiler(
-        FaustLibrary.createLibFaust(module)
-      );
-      const factory = FaustLibrary.createMonoFactory();
-      return {
-        compiler,
-        factory,
-      };
-    });
+    // this.ready = FaustModule().then((module) => {
+    //   const compiler = FaustLibrary.createCompiler(
+    //     FaustLibrary.createLibFaust(module)
+    //   );
+    //   const factory = FaustLibrary.createMonoFactory();
+    //   return {
+    //     compiler,
+    //     factory,
+    //   };
+    // });
   }
 
   async compileNode(
     audioContext: AudioContext | OfflineAudioContext,
     dsp: string
   ): Promise<FaustMonoNode> {
-    const { compiler, factory } = await this.ready;
+    // const { compiler, factory } = await this.ready;
 
-    // const start = Date.now();
+    // // const start = Date.now();
 
-    const node = await factory.compileNode(
-      audioContext,
-      "Faust",
-      compiler,
-      dsp,
-      "-ftz 2",
-      false,
-      128
+    // const node = await factory.compileNode(
+    //   audioContext,
+    //   "Faust",
+    //   compiler,
+    //   dsp,
+    //   "-ftz 2",
+    //   false,
+    //   128
+    // );
+
+    // // const end = Date.now();
+    // // console.log("compilation time", end - start, "ms");
+
+    // if (!node) throw new Error("couldnt create dsp node");
+    // return node;
+
+    const argv = ["-ftz", "2", "-I", "http://127.0.0.1:8000/../../libraries/"];
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const { faust } = window;
+
+    // Dynamically create the Faust generated node from explicit DSP source in 'dsp_code'
+    const factory = await new Promise((resolve) =>
+      faust.createDSPFactory(dsp, argv, resolve)
     );
 
-    // const end = Date.now();
-    // console.log("compilation time", end - start, "ms");
+    const node = await new Promise((resolve) =>
+      faust.createDSPWorkletInstance(factory, audioContext, resolve)
+    );
 
-    if (!node) throw new Error("couldnt create dsp node");
+    // throw new Error("couldnt create dsp node");
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
     return node;
   }
 
   async renderOffline(
-    dsp: string,
-    sampleRate: number,
-    bufferSize: number,
-    input?: Float32Array[]
+    _dsp: string,
+    _sampleRate: number,
+    _bufferSize: number,
+    _input?: Float32Array[]
   ) {
-    const { compiler } = await this.ready;
-    const factory = await compiler.createMonoDSPFactory(
-      "Faust",
-      dsp,
-      "-I libraries"
-    );
-
-    if (!factory) throw new Error("couldnt create factory");
-
-    const offline =
-      await FaustLibrary.createMonoFactory().createOfflineProcessor(
-        factory,
-        sampleRate,
-        bufferSize,
-        input
-      );
-
-    if (!offline) throw new Error("couldnt create offline processor");
-
-    return offline.plot(bufferSize);
+    // const { compiler } = await this.ready;
+    // const factory = await compiler.createMonoDSPFactory(
+    //   "Faust",
+    //   dsp,
+    //   "-I libraries"
+    // );
+    // if (!factory) throw new Error("couldnt create factory");
+    // const offline =
+    //   await FaustLibrary.createMonoFactory().createOfflineProcessor(
+    //     factory,
+    //     sampleRate,
+    //     bufferSize,
+    //     input
+    //   );
+    // if (!offline) throw new Error("couldnt create offline processor");
+    // return offline.plot(bufferSize);
   }
 }
