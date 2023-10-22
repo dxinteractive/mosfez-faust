ef768f020a61a5ce2ae08e4e5af4be15bd09e059
dxinteractive <dxinteractive@gmail.com>
2023-10-22 18:37:40 +1100

    echo loop

--- dev/src/dsp-definitions/07-param-layers.ts
+++ dev/src/dsp-definitions/07-param-layers.ts
@@ -3,23 +3,26 @@ import type { DspDefinition } from "../types";
 const dsp = `
 import("stdfaust.lib");
 
+isFirstTick = ba.time == 0;
+sAndHWithDefault(default, trig, x) = ba.sAndH(isFirstTick | trig, select2(isFirstTick, x, default));
+
 changed(x) = x != x';
 
 layerIsAwake(l,x,t) = return with {
-    layerChange = l : changed;
-    startPos = x : ba.sAndH(layerChange);
-    nudged = abs(x - startPos) > t;
-    return = layerChange,nudged : ba.on_and_off : _ == 0;
+  layerChange = l : changed;
+  startPos = x : ba.sAndH(layerChange);
+  nudged = abs(x - startPos) > t;
+  return = layerChange,nudged : ba.on_and_off : _ == 0;
 };
 
-layerValue(l,i,x) = return with {
-    return = x : ba.sAndH((l == i) & layerIsAwake(l,knob,0.1));
+layerValue(l,i,t,x) = return with {
+  return = x : sAndHWithDefault(0.5, (l == i) & layerIsAwake(l,knob,t));
 };
 
 layer = hslider("layer", 0.0, 0.0, 2.0, 1.0);
 knob = hslider("knob", 0.0, 0.0, 1.0, 0.001);
 
-layerOutput(i) = layerValue(layer,i,knob): hbargraph("value %i", 0.0, 1.0);
+layerOutput(i) = layerValue(layer,i,0.1,knob): hbargraph("value %i", 0.0, 1.0);
 
 process = par(i, 3, layerOutput(i));
 `;
