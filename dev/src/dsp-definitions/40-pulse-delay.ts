d09270265cfae50d89b33157f0af959a194760c5
dxinteractive <dxinteractive@gmail.com>
2024-07-11 11:16:02 +1000

    stuff

--- dev/src/dsp-definitions/40-pulse-delay.ts
+++ dev/src/dsp-definitions/40-pulse-delay.ts
@@ -6,9 +6,9 @@ import("stdfaust.lib");
 delay_max = ma.SR * 5;
 
 wet_param = 1.0;
-dry_param = 0.7;
-fb_param = 0.9;
-time_param = 0.2 * ma.SR;
+dry_param = 1.0;
+fb_param = 0.8;
+time_param = 1.0 * ma.SR;
 
 squash(x) = 1.0 - (1.0 / (x + 1.0));
 stretch(mn, mx, x) = (x - mn) / (mx - mn) : min(1.0) : max(0.0);
@@ -19,7 +19,7 @@ mod_speed_param = 3.0;
 mod_depth_param = 0.0002;
 mod = 1.0 - ((os.osc(mod_speed_param) * 0.5 + 0.5) * mod_depth_param);
 
-delay(time, fb, x) = x + fb : de.fdelay(delay_max, time * mod) : fi.highpass(2, 200.0) : fi.lowpass(2, 5000.0);
+delay(time, fb, x) = x + fb : de.fdelay(delay_max, time * mod) : fi.highpass(2, 200.0) : fi.lowpass(2, 2000.0);
 fb = *(fb_param);
 echo(time) = delay(time) ~ fb;
 
