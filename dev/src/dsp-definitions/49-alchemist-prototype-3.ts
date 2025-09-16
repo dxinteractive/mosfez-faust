56df7ddccd403870f64f1134a23f47dac4438ca1
Damien Clarke <dxinteractive@gmail.com>
2025-09-16 13:31:53 +1000

    ugh

--- dev/src/dsp-definitions/49-alchemist-prototype-3.ts
+++ dev/src/dsp-definitions/49-alchemist-prototype-3.ts
@@ -6,11 +6,11 @@ import("stdfaust.lib");
 // synth
 
 tune1 = waveform{59,-1,57,-1,59,-1,62,-1,57,-1,59,62,59,-1,57,-1};
-tune2 = waveform{66,-1,-1,-1,-1,-1,-1,-1,66,-1,-1,-1,-1,-1,-1,-1};
+tune2 = waveform{47,-1,-1,42,-1,-1,45,-1,50,-1,-1,42,-1,-1,45,-1};
 tunePlayer(tune) = tuneGate,tuneFreq
 with {
     tuneNow = tune,int(os.phasor(16,0.2)) : rdtable;
-    tuneGate = tuneNow > 0 : ba.sAndH(tuneNow != 0) : en.adsre(0.01,1.3,0.0,1.0);
+    tuneGate = tuneNow > 0 : ba.sAndH(tuneNow != 0) : en.adsre(0.01,3.,0.0,2.0);
     tuneFreq = tuneNow : ba.sAndH(tuneNow > 0);
 };
 synth(gate) = ba.midikey2hz : os.triangle : *(gate) : *(0.2);
@@ -62,20 +62,33 @@ layerValue(l,i,t,x) = return with {
   return = x : sAndHWithDefault(0.5, (l == i) & layerIsAwake(l,x,t));
 };
 
+sine(rate) = os.osc(rate) + 1. * .5;
+
 // consts
 
+chorus_voice_count = 5;
 // ...
 
 // input
 
-lag_param = hslider("lag[OWL:A]", 0.5, 0.0, 1.0, 0.001) : reject_noise(0.05, 1.0) : si.smoo;
-shape_param = hslider("shape[OWL:B]", 0, 0, shape_count, 1) : int;
-trem_param = hslider("trem[OWL:D]", 0.5, 0.0, 1.0, 0.001) : reject_noise(0.05, 1.0) : si.smoo;
-depth_param = hslider("depth[OWL:C]", 0.5, 0.0, 1.0, 0.001) : reject_noise(0.05, 1.0) : si.smoo;
-width_button = button("width[OWL:B1]");
-alt_button = button("alt[OWL:B2]");
+// lag_param = hslider("lag[OWL:A]", 0.5, 0.0, 1.0, 0.001) : reject_noise(0.05, 1.0) : si.smoo;
+// shape_param = hslider("shape[OWL:B]", 0, 0, shape_count, 1) : int;
+// trem_param = hslider("trem[OWL:D]", 0.5, 0.0, 1.0, 0.001) : reject_noise(0.05, 1.0) : si.smoo;
+// depth_param = hslider("depth[OWL:C]", 0.5, 0.0, 1.0, 0.001) : reject_noise(0.05, 1.0) : si.smoo;
+// width_button = button("width[OWL:B1]");
+// alt_button = button("alt[OWL:B2]");
 
-gtr = _ <: _,_;
+// effects
+
+// chorus_offset(i) = sine(5.0 + i) + sine(2.3 + i * .5) + sine(5.7 + i * .2) : /(3.);
+
+chorus_offset(i) = sine(5.0 + i);
+chorus_voice(i) = de.fdelay(3000, chorus_offset(i * .1) * 10.) <: _,_;
+chorus(x) = x <: par(i, chorus_voice_count, chorus_voice(i)) :> /(chorus_voice_count),/(chorus_voice_count);
+
+// routing
+
+gtr = _ : chorus : _,_;
 bass =  _ <: _,_;
 
 amp = *(3.0);
