83dd0620b2943abdf47ea7f8a1155fa24b14d5f3
Damien Clarke <dxinteractive@gmail.com>
2025-11-09 23:37:20 +1100

    sdfdsfds

--- dev/src/dsp-definitions/49-alchemist-prototype-3.ts
+++ dev/src/dsp-definitions/49-alchemist-prototype-3.ts
@@ -26,12 +26,13 @@ bass_synth = tunePlayer(tune2) : synth;
 counter(trig) = upfront(trig) : + ~ _ with { upfront(x) = x > x'; };
 
 lerp(a, b, x) = a + (b - a) * x;
+unlerp(a, b, x) = (x - a) / (b - a);
 
-constantPowerPan(p, x) = x*gainLeft, x*gainRight
+constantPowerPan(p, x, y) = x*gainLeft, y*gainRight
 with {
     theta = ma.PI*p/2.;
-    gainLeft = cos(theta)/sqrt(2.);
-    gainRight = sin(theta)/sqrt(2.);
+    gainLeft = cos(theta)/sqrt(2.)*2.;
+    gainRight = sin(theta)/sqrt(2.)*2.;
 };
 
 reject_noise(slack, move_time, x) = return with {
@@ -80,9 +81,6 @@ width_param = hslider("width[OWL:D]", 0., 0., 1., .001) : reject_noise(0.05, 1.0
 detune_on = button("detune_on[OWL:B1]") : ba.toggle;
 trem_on = button("trem_on[OWL:B2]") : ba.toggle;
 
-// TODO sidechain
-// TODO width options
-
 // fx
 wet_amount_detune = ba.if(detune_param < 2/3, ba.if(detune_param < 1/3, .1, .3), .5);
 wet_amount = ba.if(detune_on, wet_amount_detune, 1.);
@@ -100,7 +98,63 @@ gtr_trem_gain = lerp(1. - trem_depth, 1. + trem_depth, os.osc(trem_speed) * .5 +
 gtr_trem_l = *(gtr_trem_gain);
 gtr_trem = ba.bypass1(trem_on : ==(0), gtr_trem_l);
 
-gtr = *(wet_amount) : gtr_trem <: gtr_detune : _,_;
+gtr_sidechain = ba.lin2LogGain(1. - sidechain_param);
+gtr_gain_amount = wet_amount * gtr_sidechain;
+gtr_gain = *(gtr_gain_amount);
+
+// stereo-wide / stereo-narrow / mono-panned / mono-autopan / offset / pingpong
+
+gtr_width(l,r) = return with {
+  p(a,b,l,r) = return with {
+    gain = (width_param >= a) & (width_param < b);
+    return = l * gain,r * gain;
+  };
+
+  p0 = 0.;
+  p1 = .07;
+  p2 = .13;
+  p3 = .2;
+  p4 = .4;
+  p5 = .6;
+  p6 = .8;
+  p7 = 1.;
+
+  p55 = (p5 + p6) * .5;
+  p65 = (p6 + p7) * .5;
+
+  pan_amount_static = unlerp(p2,p4,width_param);
+  pan_amount_auto = os.osc(unlerp(p4,p5,width_param) : lerp(.01, .5)) * .5 + .5;
+  pan_amount = ba.if(width_param < p4, pan_amount_static, pan_amount_auto);
+
+  del_max = ma.SR;
+
+  d(a,b,x) = return with {
+    gain = (width_param >= a) & (width_param < b);
+    return = x * gain;
+  };
+
+  del_la = unlerp(p55,p6,width_param) : lerp(0.,del_max) : d(p55,p6);
+  del_lb = unlerp(p6,p65,width_param) : lerp(del_max * .5, 0.) : d(p6,p65);
+  del_lc = unlerp(p65,p7,width_param) : lerp(0.,del_max) : d(p65,p7);
+  del_l = del_la + del_lb + del_lc;
+
+  del_ra = unlerp(p5,p55,width_param) : lerp(del_max, 0.) : d(p5,p55);
+  del_rb = unlerp(p6,p65,width_param) : lerp(del_max, 0.) : d(p6,p65);
+  del_rc = unlerp(p65,p7,width_param) : lerp(0., del_max * .5) : d(p65,p7);
+  del_r = del_ra + del_rb + del_rc;
+
+  del_vol = ba.if(width_param < p6, 1., .5);
+
+  stereo = p(p0,p1);
+  narrow(l,r) = l*.75+r*.25,r*.75+l*.25 : p(p1,p2);
+  mono = _,! <: p(p2,p3);
+  pan = constantPowerPan(pan_amount) : p(p3,p5);
+  offset = de.delay(del_max,del_l),de.delay(del_max,del_r) : *(del_vol),*(del_vol) : p(p5,p7);
+  
+  return = l,r <: stereo,narrow,mono,pan,offset :> _,_;
+};
+
+gtr = gtr_gain : gtr_trem <: gtr_detune : gtr_width;
 
 bass_gate = ef.gate_mono(-64., .005, 0., 1.);
 bass_comp = co.compressor_mono(32., -34., 0., .4);
@@ -131,16 +185,10 @@ export default dspDefinition;
  * trem = - depth 30%: rate .2Hz ... 10Hz
  *        - depth 60%: rate .2Hz ... 10Hz
  *        - depth 100%: rate .2Hz ... 10Hz
- *        - ??? randomise depth?
  *
- * width = NOT IMPLEMENTED
- *         - straight: mono ... stereo
- *         - autopan: slow ... fast
- *         - offset: 0ms ... 500ms
- *         - ping pong: 0ms ... 500ms
+ * width = various
  *
- * sidechain = NOT IMPLEMENTED
- *             - volume decrease: 0 ... 100%
+ * sidechain = - volume decrease: 0 ... 100%
  *
  * BUTTONS:
  *
